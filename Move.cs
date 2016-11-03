5087978408980f9fa9314f663887fdd8107e6069
Ys-PC\Ys <kkys99>
2016-11-03 11:18:36 +0900

    unity내 케릭터 움직임

---
+++ Move.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour {
+    
+    public CharacterController CC;
+	Vector3 V3;
+	float MoveSpeed;
+	float TurnSpeed;
+
+	// Use this for initialization
+	void Start () {
+		MoveSpeed = 7;
+		TurnSpeed = 3f;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		V3 = new Vector3 (0, Input.GetAxis ("Mouse X"), 0);
+		transform.Rotate (V3 * TurnSpeed);
+
+		if (Input.GetKey (KeyCode.W)) {
+			CC.Move (transform.forward * MoveSpeed * Time.deltaTime);
+		}
+		if (Input.GetKey (KeyCode.S)) {
+			CC.Move (transform.forward * -1f * MoveSpeed * Time.deltaTime);
+        }
+		if (Input.GetKey (KeyCode.A)) {
+			CC.Move (transform.right * -1f *MoveSpeed * Time.deltaTime);
+        }
+		if (Input.GetKey (KeyCode.D)) {
+			CC.Move (transform.right * MoveSpeed * Time.deltaTime);
+        }
+	
+	}
+}
