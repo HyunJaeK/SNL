9d1b1a58dfbdac704c12c0e90fd113c858d5a68c
Ys-PC\Ys <kkys99>
2016-11-07 22:31:34 +0900

    총 발포시 총알생성 제거

---
+++ FireEffect.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireEffect : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Invoke ("OFF", 0.01f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		gameObject.SetActive (false);
+	}
+}
