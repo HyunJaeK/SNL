e2f20af703d473b6d7b208e868520cb559067c5f
Ys-PC\Ys <kkys99>
2016-11-09 00:33:53 +0900

    적 사살

---
+++ Jojun.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Jojun : MonoBehaviour {
+
+	public Transform jjojun;
+	RaycastHit Hit;
+	// Use this for initialization
+	public IEnumerator JoJunHam(){
+		Physics.Raycast (jjojun.position, jjojun.forward, out Hit, 10000f);
+		if(Hit.collider)
+		{
+			if(Hit.collider.tag == "Monster")
+			{
+				Destroy(Hit.collider.gameObject);
+			}
+		}
+		yield return null;
+	}
+}
