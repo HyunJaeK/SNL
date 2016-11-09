e81cc5c327d1fb4d830c3afc1387aa059d2719c3
Ys-PC\Ys <kkys99>
2016-11-09 23:52:24 +0900

    마우스 화면제어

---
+++ Camera.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera : MonoBehaviour {
+	
+	Vector3 V3;
+	float TurnSpeed;
+    float X;
+	// Use this for initialization
+	void Start () {
+		TurnSpeed = 3f;
+        Screen.lockCursor = true;
+	}
+    void OnMouseDown()
+    {
+        Screen.lockCursor = true;
+    }
+
+	// Update is called once per frame
+	void Update () {
+        X = -Input.GetAxis("Mouse Y");
+        if(transform.rotation.eulerAngles.x+X <=40f || transform.rotation.eulerAngles.x+X >= 320f)
+        {
+            V3 = new Vector3(X, 0, 0);
+            transform.Rotate(V3 * TurnSpeed);
+        }
+	}
+}
