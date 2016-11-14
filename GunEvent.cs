b9da7539a025f46d8d7a534bd73fb54eb099c47e
Ys-PC\Ys <kkys99>
2016-11-14 22:01:20 +0900

    튜토리얼 내 총 오브젝트 코드

---
+++ GunEvent.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GunEvent : MonoBehaviour {
+
+	public GameObject GunObject;
+
+	void GunEventON(){
+		GunObject.GetComponent<Fire> ().GunFireONEvent ();
+	}
+	void GunEventOFF(){
+		GunObject.GetComponent<Fire> ().GunFireOFFEvent ();
+	}
+
+}
